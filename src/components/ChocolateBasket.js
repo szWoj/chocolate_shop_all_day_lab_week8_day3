dce226174ab4ae3504f6fc252b670796f9295f52
Szymon <wojdyla.szymon@gmail.com>
2022-04-13 12:21:55 +0100

    half way through initail set up

---
+++ src/components/ChocolateBasket.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ChocolateBasket = () => {
+
+    
+}
+
+export default ChocolateBasket
---
+++ src/containers/MainChocolateContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ChocolateList from '../components/ChocolateList';
+import ChocolateBasket from '../components/ChocolateBasket';
+
+
+const MainChocolateContainer = () => {
+
+    return(
+        <>
+        <ChocolateList />
+        <ChocolateBasket />
+        </>
+    )
+
+}
+
+export default MainChocolateContainer;
